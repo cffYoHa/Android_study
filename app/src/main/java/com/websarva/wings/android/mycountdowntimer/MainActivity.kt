29b5e49c55110d68bfe57e35dddce3c8b4176bf2
Shunta Hanyo <b1018162.fun@gmail.com>
2020-08-22 18:10:56 +0900

    [add]:はじプロ/カウントダウンタイマー処理の実装

--- app/src/main/java/com/websarva/wings/android/mycountdowntimer/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/mycountdowntimer/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.websarva.wings.android.mycountdowntimer
 
+import android.content.IntentSender
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    //innerがつくことで、外部の変数（レイアウトに配置されたビューのID名を参照できるようになる
+    inner class  MyCountDownTimer(millisInFuture: Long, countDownInterval: Long) :
+        CountDownTimer(millisInFuture, countDownInterval) {
+
+        var isRunnig = false
+
+        override fun onTick(millisUntilFinished : Long) {
+            val minute = millisUntilFinished / 1000L / 60L
+            val second = millisUntilFinished / 1000L % 60L
+            timerText.text = "%1d:%2$02d".format(minute, second)
+        }
+
+        override fun onFinish() {
+            timerText.text = "0:00"
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        timerText.text = "3:00"
+        val timer = MyCountDownTimer(3 * 60 * 1000, 100)
+        playStop.setOnClickListener {
+            timer.isRunnig = when (timer.isRunnig) {
+                true -> {
+                    timer.cancel()
+                    playStop.setImageResource(
+                        R.drawable.ic_baseline_play_arrow_24
+                    )
+                    false
+                }
+                false -> {
+                    timer.start()
+                    playStop.setImageResource(
+                        R.drawable.ic_baseline_stop_24
+                    )
+                    true
+                }
+            }
+        }
     }
 }
